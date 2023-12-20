4d2d9f73eaf6347acc9a01d069da90877252cb3b
N3xKxp3r <154423326+N3xKxp3r@users.noreply.github.com>
2023-12-20 11:23:11 +0100

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,12 +1,29 @@
-local Save = require(game.ReplicatedStorage.Library.Client.Save).Get()
+-- Made By N3xKxp3r
 
-local function GetDiamonds()
-    for _, v in pairs(Save.Inventory.Currency) do 
-        if v.id == 'Diamonds' then 
-            return v._am 
-        end 
+local SaveModule = require(game.ReplicatedStorage.Library.Client.Save)
+local a = SaveModule.Get()
+
+local function formatNumber(number)
+    local suffixes = {"", "k", "m", "b", "t", "q", "qa"}
+    local suffixIndex = 1
+
+    while number >= 1000 and suffixIndex < #suffixes do
+        number = number / 1000
+        suffixIndex = suffixIndex + 1
     end
+
+    return string.format("%.2f%s", number, suffixes[suffixIndex])
 end
 
-local diamonds = GetDiamonds()
-print(diamonds)
+local function getDiamondsAmount()
+    for _, currency in pairs(a.Inventory.Currency) do
+        if currency.id == 'Diamonds' then
+            return currency._am
+        end
+    end
+    return 0
+end
+
+local diamondsAmount = getDiamondsAmount()
+local formattedDiamonds = formatNumber(diamondsAmount)
+print("Diamonds: " .. formattedDiamonds)
