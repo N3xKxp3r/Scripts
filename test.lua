1afc40833373094f9c4ba0deb2d9ad387f147bf4
N3xKxp3r <154423326+N3xKxp3r@users.noreply.github.com>
2023-12-20 14:46:06 +0100

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,6 +1,150 @@
 getgenv().Config = {
-    ["Pets"] = {
-        ["Prince Donkey"] = 10000,
+    ["Items"] = {
+        ["Prince Donkey"] = {price = 15, maxAmount = 1000},
+        ["Princess Dragon"] = {price = 15, maxAmount = 1000},
+        ["Fortune"] = {price = 15, maxAmount = 1000},
+        ["Charm Stone"] = {price = 15, maxAmount = 1000},
+        ["Titanic Christmas Present"] = {price = 15, maxAmount = 1000},
+        ["Medium Christmas"] = {price = 15, maxAmount = 1000},
+        ["Large Christmas Present"] = {price = 15, maxAmount = 1000},
+        ["X-Large Christmas Present"] = {price = 15, maxAmount = 1000},
+        ["Small Christmas Present"] = {price = 15, maxAmount = 1000},
+        ["Hot Cocoa"] = {price = 15, maxAmount = 1000},
+        ["Candycane"] = {price = 15, maxAmount = 1000},
+        ["Christmas Cookie"] = {price = 15, maxAmount = 1000},
+        ["Fortune Flag"] = {price = 15, maxAmount = 1000},
+        ["Diamonds Flag"] = {price = 15, maxAmount = 1000},
+        ["Coins Flag"] = {price = 15, maxAmount = 1000},
+        ["Magnet Flag"] = {price = 15, maxAmount = 1000},
+        ["Hasty Flag"] = {price = 15, maxAmount = 1000},
+        ["Royalty Charm"] = {price = 15, maxAmount = 1000},
+        ["Glittering Charm"] = {price = 15, maxAmount = 1000},
+        ["Strength Charm"] = {price = 15, maxAmount = 1000},
+        ["Diamonds Charm"] = {price = 15, maxAmount = 1000},
+        ["Coins Charm"] = {price = 15, maxAmount = 1000},
+        ["Bonus Charm"] = {price = 15, maxAmount = 1000},
+        ["Insta-Plant Capsule"] = {price = 15, maxAmount = 1000},
+        ["Golden Watering Can"] = {price = 15, maxAmount = 1000},
+        ["Banana"] = {price = 15, maxAmount = 1000},
+        ["Apple"] = {price = 15, maxAmount = 1000},
+        ["Pineapple"] = {price = 15, maxAmount = 1000},
+        ["Orange"] = {price = 15, maxAmount = 1000},
+        ["Golden Shovel"] = {price = 15, maxAmount = 1000},
+        ["Golden Fishing Rod"] = {price = 15, maxAmount = 1000},
+        ["Toy Bone"] = {price = 15, maxAmount = 1000},
+        ["Toy Ball"] = {price = 15, maxAmount = 1000},
+        ["Clan Voucher"] = {price = 15, maxAmount = 1000},
+        ["Rainbow Fruit"] = {price = 15, maxAmount = 1000},
+        ["Gift Bag"] = {price = 15, maxAmount = 1000},
+        ["Large Gift Bag"] = {price = 15, maxAmount = 1000},
+        ["Agility Charm"] = {price = 15, maxAmount = 1000},
+        ["Spinny Wheel Ticket"] = {price = 15, maxAmount = 1000},
+        ["Booth Slot Voucher"] = {price = 15, maxAmount = 1000},
+        ["Magic Coin Jar"] = {price = 15, maxAmount = 1000},
+        ["Crystal Key"] = {price = 15, maxAmount = 1000},
+        ["Crystal Key: Upper Half"] = {price = 15, maxAmount = 1000},
+        ["Crystal Key: Lower Half"] = {price = 15, maxAmount = 1000},
+        ["Basic Coin Jar"] = {price = 15, maxAmount = 1000},
+        ["TNT"] = {price = 15, maxAmount = 1000},
+        ["Comet"] = {price = 15, maxAmount = 1000},
+        ["Giant Coin Jar"] = {price = 15, maxAmount = 1000},
+        ["Squeaky Toy"] = {price = 15, maxAmount = 1000},
+        ["TNT Crate"] = {price = 15, maxAmount = 1000},
+        ["Nametag"] = {price = 15, maxAmount = 1000},
+        ["Coins I"] = {price = 15, maxAmount = 1000},
+        ["Coins II"] = {price = 15, maxAmount = 1000},
+        ["Coins III"] = {price = 15, maxAmount = 1000},
+        ["Coins IV"] = {price = 15, maxAmount = 1000},
+        ["Coins V"] = {price = 15, maxAmount = 1000},
+        ["Tap Power I"] = {price = 15, maxAmount = 1000},
+        ["Tap Power II"] = {price = 15, maxAmount = 1000},
+        ["Tap Power III"] = {price = 15, maxAmount = 1000},
+        ["Tap Power IV"] = {price = 15, maxAmount = 1000},
+        ["Tap Power V"] = {price = 15, maxAmount = 1000},
+        ["Criticals I"] = {price = 15, maxAmount = 1000},
+        ["Criticals II"] = {price = 15, maxAmount = 1000},
+        ["Criticals III"] = {price = 15, maxAmount = 1000},
+        ["Criticals IV"] = {price = 15, maxAmount = 1000},
+        ["Criticals V"] = {price = 15, maxAmount = 1000},
+        ["Strong Pets I"] = {price = 15, maxAmount = 1000},
+        ["Strong Pets II"] = {price = 15, maxAmount = 1000},
+        ["Strong Pets III"] = {price = 15, maxAmount = 1000},
+        ["Strong Pets IV"] = {price = 15, maxAmount = 1000},
+        ["Strong Pets V"] = {price = 15, maxAmount = 1000},
+        ["Treasure Hunter I"] = {price = 15, maxAmount = 1000},
+        ["Treasure Hunter II"] = {price = 15, maxAmount = 1000},
+        ["Treasure Hunter III"] = {price = 15, maxAmount = 1000},
+        ["Treasure Hunter IV"] = {price = 15, maxAmount = 1000},
+        ["Treasure Hunter V"] = {price = 15, maxAmount = 1000},
+        ["Magnet I"] = {price = 15, maxAmount = 1000},
+        ["Magnet II"] = {price = 15, maxAmount = 1000},
+        ["Magnet III"] = {price = 15, maxAmount = 1000},
+        ["Speed I"] = {price = 15, maxAmount = 1000},
+        ["Speed II"] = {price = 15, maxAmount = 1000},
+        ["Speed III"] = {price = 15, maxAmount = 1000},
+        ["Speed IV"] = {price = 15, maxAmount = 1000},
+        ["Speed V"] = {price = 15, maxAmount = 1000},
+        ["Diamonds I"] = {price = 15, maxAmount = 1000},
+        ["Diamonds II"] = {price = 15, maxAmount = 1000},
+        ["Diamonds III"] = {price = 15, maxAmount = 1000},
+        ["Diamonds IV"] = {price = 15, maxAmount = 1000},
+        ["Diamonds V"] = {price = 15, maxAmount = 1000},
+        ["Lucky Eggs I"] = {price = 15, maxAmount = 1000},
+        ["Lucky Eggs II"] = {price = 15, maxAmount = 1000},
+        ["Lucky Eggs III"] = {price = 15, maxAmount = 1000},
+        ["Lucky Eggs IV"] = {price = 15, maxAmount = 1000},
+        ["Lucky Eggs V"] = {price = 15, maxAmount = 1000},
+        ["Starfall"] = {price = 15, maxAmount = 1000},
+        ["Super Lightning"] = {price = 15, maxAmount = 1000},
+        ["Tap Teamwork"] = {price = 15, maxAmount = 1000},
+        ["Happy Pets"] = {price = 15, maxAmount = 1000},
+        ["Lightning"] = {price = 15, maxAmount = 1000},
+        ["Blast"] = {price = 15, maxAmount = 1000},
+        ["Chest Breaker"] = {price = 15, maxAmount = 1000},
+        ["Exotic Pet"] = {price = 15, maxAmount = 1000},
+        ["Huge Hunter"] = {price = 15, maxAmount = 1000},
+        ["Lucky Block"] = {price = 15, maxAmount = 1000},
+        ["Chest Mimic Fortune"] = {price = 15, maxAmount = 1000},
+        ["Shiny Hunter"] = {price = 15, maxAmount = 1000},
+        ["Fireworks"] = {price = 15, maxAmount = 1000},
+        ["Midas Touch"] = {price = 15, maxAmount = 1000},
+        ["Speed Potion I"] = {price = 15, maxAmount = 1000},
+        ["Speed Potion II"] = {price = 15, maxAmount = 1000},
+        ["Speed Potion III"] = {price = 15, maxAmount = 1000},
+        ["Speed Potion IV"] = {price = 15, maxAmount = 1000},
+        ["Speed Potion V"] = {price = 15, maxAmount = 1000},
+        ["Speed Potion VI"] = {price = 15, maxAmount = 1000},
+        ["Treasure Hunter Potion I"] = {price = 15, maxAmount = 1000},
+        ["Treasure Hunter Potion II"] = {price = 15, maxAmount = 1000},
+        ["Treasure Hunter Potion III"] = {price = 15, maxAmount = 1000},
+        ["Treasure Hunter Potion IV"] = {price = 15, maxAmount = 1000},
+        ["Treasure Hunter Potion V"] = {price = 15, maxAmount = 1000},
+        ["Treasure Hunter Potion VI"] = {price = 15, maxAmount = 1000},
+        ["Lucky Potion I"] = {price = 15, maxAmount = 1000},
+        ["Lucky Potion II"] = {price = 15, maxAmount = 1000},
+        ["Lucky Potion III"] = {price = 15, maxAmount = 1000},
+        ["Lucky Potion IV"] = {price = 15, maxAmount = 1000},
+        ["Lucky Potion V"] = {price = 15, maxAmount = 1000},
+        ["Lucky Potion VI"] = {price = 15, maxAmount = 1000},
+        ["Diamonds Potion I"] = {price = 15, maxAmount = 1000},
+        ["Diamonds Potion II"] = {price = 15, maxAmount = 1000},
+        ["Diamonds Potion III"] = {price = 15, maxAmount = 1000},
+        ["Diamonds Potion IV"] = {price = 15, maxAmount = 1000},
+        ["Diamonds Potion V"] = {price = 15, maxAmount = 1000},
+        ["Diamonds Potion VI"] = {price = 15, maxAmount = 1000},
+        ["Damage Potion I"] = {price = 15, maxAmount = 1000},
+        ["Damage Potion II"] = {price = 15, maxAmount = 1000},
+        ["Damage Potion III"] = {price = 15, maxAmount = 1000},
+        ["Damage Potion IV"] = {price = 15, maxAmount = 1000},
+        ["Damage Potion V"] = {price = 15, maxAmount = 1000},
+        ["Damage Potion VI"] = {price = 15, maxAmount = 1000},
+        ["Coins Potion I"] = {price = 15, maxAmount = 1000},
+        ["Coins Potion II"] = {price = 15, maxAmount = 1000},
+        ["Coins Potion III"] = {price = 15, maxAmount = 1000},
+        ["Coins Potion IV"] = {price = 15, maxAmount = 1000},
+        ["Coins Potion V"] = {price = 15, maxAmount = 1000},
+        ["Coins Potion VI"] = {price = 15, maxAmount = 1000},
+        ["The Cocktail"] = {price = 15, maxAmount = 1000},
     },
     ["Settings"] = {
         ["timeFormat"] = "24h",
@@ -27,10 +171,15 @@ local function processListingInfo(uid, gems, item, version, shiny, amount, usern
     print("Sniped")
 
     local function formatTimeAndOffset(timeFormat, hourOffset)
-        return os.date(
+        local currentTimeUTC = os.time(os.date("!*t"))
+        local offsetSeconds = hourOffset * 3600
+        local adjustedTime = currentTimeUTC + offsetSeconds
+        local formattedTime = os.date(
             timeFormat == "12h" and "%I:%M:%S %p" or "%H:%M:%S",
-            os.time() + (hourOffset and hourOffset * 3600 or 0)
+            adjustedTime
         ):gsub("^%s*(.-)%s*$", "%1")
+    
+        return formattedTime
     end
 
     local SaveModule = require(game.ReplicatedStorage.Library.Client.Save)
@@ -40,24 +189,24 @@ local function processListingInfo(uid, gems, item, version, shiny, amount, usern
     local timezoneInfo = "GMT" .. (getgenv().Config.Settings.hourOffset >= 0 and "+" or "") .. getgenv().Config.Settings.hourOffset
 
     local fields = {
-        { name = "Pet Name:", value = item, inline = false },
-        { name = "Version:", value = versionText .. (shiny and " Shiny" or ""), inline = false },
-        { name = "Amount:", value = tostring(amount), inline = false },
-        { name = "Pet ID:", value = tostring(uid), inline = false },
-        { name = "Price:", value = tostring(gems) .. " Gems", inline = false },
-        { name = "Sniped From:", value = tostring(username), inline = false },
-        { name = "Alt:", value = game.Players.LocalPlayer.Name, inline = false }
+        { name = "Item Name:", value = "||**" .. item .. "**||", inline = false }, -- Change 'Pet Name' to 'Item Name'
+        { name = "Version:", value = "||**" .. (getgenv().Config.Items[item] and versionText or "N/A") .. (shiny and " Shiny" or "") .. "**||", inline = false },
+        { name = "Amount:", value = "||**" .. tostring(amount) .. "**||", inline = false },
+        { name = "Item ID:", value = "||**" .. tostring(uid) .. "**||", inline = false }, -- Change 'Pet ID' to 'Item ID'
+        { name = "Price:", value = "||**" .. tostring(gems) .. " Gems**||", inline = false },
+        { name = "Sniped From:", value = "||**" .. tostring(username) .. "**||", inline = false },
+        { name = "Alt:", value = "**" .. game.Players.LocalPlayer.Name .. "**", inline = false }
     }
 
     local message = {
-        content = "New Pet Sniped!",
+        content = "New Item Sniped!",'
         embeds = {
             {
                 title = snipeMessage,
                 fields = fields,
-                author = { name = "New Pet Sniped!" },
+                author = { name = "New Item Sniped!" },
                 footer = {
-                    text = "Sniped at " .. currentTime .. " | Timezone: " .. timezoneInfo,
+                    text = "**Sniped at " .. currentTime + 5 .. " | Timezone: " .. timezoneInfo .. "**"
                 },
             }
         },
@@ -78,10 +227,15 @@ end
 
 local function checkListing(uid, gems, item, version, shiny, amount, username, playerID)
     gems = tonumber(gems)
-    local petMaxPrice = getgenv().Config.Pets[item]
-    if petMaxPrice and gems <= petMaxPrice or gems <= 5 then
-        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerID, uid)
-        processListingInfo(uid, gems, item, version, shiny, amount, username)
+
+    if getgenv().Config.Items[item] then
+        local itemInfo = getgenv().Config.Items[item]
+
+        if gems <= itemInfo.price and amount <= itemInfo.maxAmount then
+            game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerID, uid)
+
+            processListingInfo(uid, gems, item, version, shiny, amount, username, playerID)
+        end
     end
 end
 
