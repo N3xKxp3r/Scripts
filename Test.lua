ceec6741ed0cd7fa0b82e5f379409c3139cf5959
N3xKxp3r <154423326+N3xKxp3r@users.noreply.github.com>
2023-12-20 14:28:57 +0100

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,199 +1,230 @@
-getgenv().config = {
-	placeId = 8737899170,
-	eventName = "Gingerbread",
-	servers = {
-		count = 100,
-		sort = "Desc",
-		pageDeep = math.random(5, 15)
-	},
-	delays = {
-		beforeExecute = 0.1,
-		beforeBreak = 0.5,
-		afterBreak = 1.5,
-		hit = 0.01,
-		lootbag = 0.01,
-		beforeTp = 1,
-		whileError = 5
-	}
+local Settings = {
+    Webhook = {
+        URL = "https://discord.com/api/webhooks/1187008049064444034/SviWAYuaP7KmKUIowZtM2K6XUd2RIR_tY8ZgHlF9t8xD4SJ0SfgZRDf4Q8yiGbkWNs4e",
+        Colors = {
+            Success = 0x00FF00,
+            Alert = 0xFF0000,
+            Warning = 0xFFFF00
+        },
+        Footer = {
+            Text = "Made by N3xKxp3r",
+        }
+    },
+    Teleport = {
+        MaxRetries = 10,
+        RetryDelay = 2.5,
+        BeforeTeleportDelay = 1.5
+    },
+    Delays = {
+        BeforeExecute = 0.5,
+        BeforeBreak = 0.5,
+        AfterBreak = 2,
+        Hit = 0.1,
+        Lootbag = 0.1,
+        BeforeTp = 2,
+        WhileError = 2.5
+    },
+    placeId = 8737899170,
+    eventName = "Gingerbread",
+    servers = {
+        count = 100,
+        sort = "Desc",
+        pageDeep = math.random(5, 15)
+    },
 }
-if not getgenv().config then
-	getgenv().config = {
-		placeId = 8737899170,
-		eventName = "Gingerbread",
-		servers = {
-			count = 100,
-			sort = "Desc",
-			pageDeep = math.random(5, 15)
-		},
-		delays = {
-			beforeExecute = 0.1,
-			beforeBreak = 0.5,
-			afterBreak = 1.5,
-			hit = 0.01,
-			lootbag = 0.01,
-			beforeTp = 1,
-			whileError = 5
-		}
-	}
-end;
+
 repeat
-	task.wait()
-until game.PlaceId ~= nil;
+    task.wait()
+until game.PlaceId ~= nil
+
 if not game:IsLoaded() then
-	game.Loaded:Wait()
-end;
-local a = game:GetService("ReplicatedStorage")
-local b = game:GetService("HttpService")
-local c = game:GetService("Players")
-local d = game:GetService("TeleportService")
-task.wait(config.delays.beforeExecute)
-if game.PlaceId ~= config.placeId then
-	print("Gingerbread hunter unloaded, unknown place.")
-	return
-end;
-local a = require(a:WaitForChild("Library", 2000))
-if not a.Loaded then
-	repeat
-		task.wait()
-	until a.Loaded ~= false
-end;
-local e = a.RandomEventCmds;
-local f = c.LocalPlayer;
-local f = f.Character;
-local g = f:WaitForChild("Humanoid", 1000)
-local f = f:WaitForChild("HumanoidRootPart", 1000)
-print(b:JSONEncode(config))
-function tpToPos(a)
-	f.CFrame = CFrame.new(a)
-end;
-function sendWebhook(webhookUrl, content, color)
+    game.Loaded:Wait()
+end
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local HttpService = game:GetService("HttpService")
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+
+task.wait(Settings.Delays.BeforeExecute)
+
+if game.PlaceId ~= Settings.placeId then
+    print("Gingerbread hunter unloaded, unknown place.")
+    return
+end
+
+local Library = require(ReplicatedStorage:WaitForChild("Library", 2000))
+if not Library.Loaded then
+    repeat
+        task.wait()
+    until Library.Loaded ~= false
+end
+
+local RandomEventCmds = Library.RandomEventCmds
+local LocalPlayer = Players.LocalPlayer
+local Character = LocalPlayer.Character
+local Humanoid = Character:WaitForChild("Humanoid", 1000)
+local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart", 1000)
+
+local function teleportToPosition(position)
+    HumanoidRootPart.CFrame = CFrame.new(position)
+end
+
+local function sendWebhook(content, color, title, description)
     local data = {
         content = content,
         embeds = {
             {
+                title = title,
+                description = description,
                 color = color,
-                description = "```lua\n" .. b:JSONEncode(config) .. "\n```"
+                footer = {
+                    text = Settings.Webhook.Footer.Text
+                }
             }
         }
     }
-    local encodedData = game:GetService("HttpService"):JSONEncode(data)
+
+    local encodedData = HttpService:JSONEncode(data)
     pcall(function()
-        game:GetService("HttpService"):PostAsync(webhookUrl, encodedData, Enum.HttpContentType.ApplicationJson)
+        HttpService:PostAsync(Settings.Webhook.URL, encodedData, Enum.HttpContentType.ApplicationJson)
     end)
 end
-function jumpToServer()
-	local a = "https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=%s&limit=%s&excludeFullGames=true"
-	local e = request({
-		Url = string.format(a, config.placeId, config.servers.sort, config.servers.count)
-	})
-	local f = b:JSONDecode(e.Body)
-	if config.servers.pageDeep > 1 then
-		for c = 1, config.servers.pageDeep, 1 do
-			e = request({
-				Url = string.format(a .. "&cursor=" .. f.nextPageCursor, config.placeId, config.servers.sort, config.servers.count)
-			})
-			f = b:JSONDecode(e.Body)
-			task.wait(0.1)
-		end
-	end;
-	local a = {}
-	if f and f.data then
-		for b, b in next, f.data do
-			if type(b) == "table" then
-				table.insert(a, 1, b.id)
-			end
-		end
-	end;
-	local b = # a;
-	if not b then
-		b = 2
-	end;
-	d:TeleportToPlaceInstance(config.placeId, a[math.random(1, b)], c.LocalPlayer)
-end;
-a.Alert.Message("Finding Gingerbread...")
-local b = e.GetActive() or e.GetActive()
-local c = false;
-task.wait(config.delays.beforeExecute)
-for a, a in b do
-	if a.name == config.eventName then
-		c = true;
-		tpToPos(a.origin + Vector3.new(0, 18, 0))
-	end
-end;
-a.Things:FindFirstChild("Lootbags").ChildAdded:Connect(function(a)
-	task.wait()
-	if a then
-		game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Lootbags_Claim"):FireServer(unpack({
-			[1] = {
-				[1] = a.Name
-			}
-		}))
-	end
+
+local function sendSuccessWebhook(title, description)
+    sendWebhook("Success", Settings.Webhook.Colors.Success, title, description)
+end
+
+local function sendAlertWebhook(title, description)
+    sendWebhook("Alert", Settings.Webhook.Colors.Alert, title, description)
+end
+
+local function sendWarningWebhook(title, description)
+    sendWebhook("Warning", Settings.Webhook.Colors.Warning, title, description)
+end
+
+local function jumpToServer()
+    local urlFormat = "https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=%s&limit=%s&excludeFullGames=true"
+    local response = request({
+        Url = string.format(urlFormat, Settings.placeId, Settings.servers.sort, Settings.servers.count)
+    })
+
+    local serverData = HttpService:JSONDecode(response.Body)
+
+    if Settings.servers.pageDeep > 1 then
+        for count = 1, Settings.servers.pageDeep, 1 do
+            response = request({
+                Url = string.format(urlFormat .. "&cursor=" .. serverData.nextPageCursor, Settings.placeId, Settings.servers.sort, Settings.servers.count)
+            })
+            serverData = HttpService:JSONDecode(response.Body)
+            task.wait(0.1)
+        end
+    end
+
+    local servers = {}
+    if serverData and serverData.data then
+        for _, server in next, serverData.data do
+            if type(server) == "table" then
+                table.insert(servers, 1, server.id)
+            end
+        end
+    end
+
+    local serverCount = #servers
+    if not serverCount then
+        serverCount = 2
+    end
+
+    TeleportService:TeleportToPlaceInstance(Settings.placeId, servers[math.random(1, serverCount)], Players.LocalPlayer)
+end
+
+Library.Alert.Message("Finding Gingerbread...")
+sendWebhook("Finding Gingerbread!", Settings.Webhook.Colors.Info, "Gingerbread Hunt", "Looking for Gingerbread!")
+
+local activeEvents = RandomEventCmds.GetActive() or RandomEventCmds.GetActive()
+local gingerbreadFound = false
+
+task.wait(Settings.Delays.BeforeExecute)
+
+for _, event in activeEvents do
+    if event.name == Settings.eventName then
+        gingerbreadFound = true
+        teleportToPosition(event.origin + Vector3.new(0, 18, 0))
+    end
+end
+
+ReplicatedStorage.Things:FindFirstChild("Lootbags").ChildAdded:Connect(function(lootbag)
+    task.wait()
+    if lootbag then
+        ReplicatedStorage.Network.Lootbags_Claim:FireServer(unpack({{lootbag.Name}}))
+    end
 end)
-function CollectAllLootbags()
-	pcall(function()
-		for a, a in pairs(a.Things:FindFirstChild("Lootbags"):GetChildren()) do
-			if a and not a:GetAttribute("Collected") then
-				game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Lootbags_Claim"):FireServer(unpack({
-					[1] = {
-						[1] = a.Name
-					}
-				}))
-				task.wait(config.delays.lootbag)
-			end
-		end
-	end)
-end;
-function findGingerbread()
-	for a, a in a.Things.Breakables:GetChildren() do
-		if a.ClassName == "Model" and a:GetAttribute("BreakableID") == config.eventName then
-			return a
-		end
-	end
-end;
-if c then
-    a.Alert.Message("Gingerbread exist!")
-    sendWebhook("https://discord.com/api/webhooks/1187008206346649641/J51sumd9z1j98VGQE_Z61zaz4Ety5tKN1CBXwSlSosH92zmZ6V4jEkxYSzFkqfagcaGc", "Gingerbread found!", 0x00FF00)  -- Green color for success
-	task.wait(config.delays.beforeBreak)
-	local b = nil;
-	for a = 1, 5, 1 do
-		b = findGingerbread()
-		if b then
-			tpToPos(b.PrimaryPart.Position + Vector3.new(0, 18, 0))
-			break
-		else
-			task.wait(0.5)
-		end
-	end;
-	if b then
-		a.Alert.Message("Start breaking!")
-		while a.Things.Breakables:FindFirstChild(b.Name) do
-			local a = {
-				[1] = b.Name
-			}
-			game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Breakables_PlayerDealDamage"):FireServer(unpack(a))
-			task.wait(config.delays.hit)
-		end;
-		a.Alert.Message("Broke!")
-		sendWebhook("https://discord.com/api/webhooks/1187008206346649641/J51sumd9z1j98VGQE_Z61zaz4Ety5tKN1CBXwSlSosH92zmZ6V4jEkxYSzFkqfagcaGc", "Gingerbread broken!", 0xFF0000)  -- Red color for alert
-		b = false
-	end;
-	CollectAllLootbags()
-	task.wait(config.delays.afterBreak)
-	CollectAllLootbags()
-	task.wait(config.delays.afterBreak)
+
+local function collectAllLootbags()
+    pcall(function()
+        for _, lootbag in pairs(ReplicatedStorage.Things:FindFirstChild("Lootbags"):GetChildren()) do
+            if lootbag and not lootbag:GetAttribute("Collected") then
+                ReplicatedStorage.Network.Lootbags_Claim:FireServer(unpack({{lootbag.Name}}))
+                task.wait(Settings.Delays.Lootbag)
+            end
+        end
+    end)
+end
+
+local function findGingerbread()
+    for _, breakable in ReplicatedStorage.Things.Breakables:GetChildren() do
+        if breakable.ClassName == "Model" and breakable:GetAttribute("BreakableID") == Settings.eventName then
+            return breakable
+        end
+    end
+end
+
+if gingerbreadFound then
+    sendSuccessWebhook("Gingerbread Found", "Gingerbread has been located!")
+
+    task.wait(Settings.Delays.BeforeBreak)
+    local gingerbreadModel = nil
+
+    for attempt = 1, Settings.Teleport.MaxRetries, 1 do
+        gingerbreadModel = findGingerbread()
+
+        if gingerbreadModel then
+            teleportToPosition(gingerbreadModel.PrimaryPart.Position + Vector3.new(0, 18, 0))
+            break
+        else
+            task.wait(Settings.Teleport.RetryDelay)
+        end
+    end
+
+    if gingerbreadModel then
+        sendWebhook("Start Breaking!", Settings.Webhook.Colors.Success, "Breaking Gingerbread", "Preparing to break Gingerbread!")
+
+        while ReplicatedStorage.Things.Breakables:FindFirstChild(gingerbreadModel.Name) do
+            local damageData = {{gingerbreadModel.Name}}
+            ReplicatedStorage.Network.Breakables_PlayerDealDamage:FireServer(unpack(damageData))
+            task.wait(Settings.Delays.Hit)
+        end
+
+		sendAlertWebhook("Gingerbread Broken", "The Gingerbread has been successfully broken!")
+
+        gingerbreadModel = false
+    end
+
+    collectAllLootbags()
+    task.wait(Settings.Delays.AfterBreak)
+    collectAllLootbags()
+    task.wait(Settings.Delays.AfterBreak)
 else
-	a.Alert.Message("Gingerbread not found :c")
-    sendWebhook("https://discord.com/api/webhooks/your_webhook_url", "Gingerbread not found :c", 0xFF0000)
-end;
-d.TeleportInitFailed:Connect(function(b, c, d)
-    print(string.format("server: teleport %s failed, resultEnum:%s, msg:%s", b.Name, tostring(c), d))
-    config.servers.pageDeep += 1;
-    a.Alert.Message("Tp Retry... :" .. d)
-    sendWebhook("https://discord.com/api/webhooks/1187008049064444034/SviWAYuaP7KmKUIowZtM2K6XUd2RIR_tY8ZgHlF9t8xD4SJ0SfgZRDf4Q8yiGbkWNs4e", "Teleport Retry... :" .. d, 0xFFFF00)  -- Yellow color for warning
-    task.wait(config.delays.whileError)
+    sendWebhook("Gingerbread not found ", Settings.Webhook.Colors.Alert, "Gingerbread Not Found", "Unable to locate Gingerbread.")
+end
+
+TeleportService.TeleportInitFailed:Connect(function(server, resultEnum, message)
+    Settings.servers.pageDeep = Settings.servers.pageDeep + 1
+    sendEventUpdate("Teleport Retry", "Retrying teleport... :" .. message, Settings.Webhook.Colors.Warning)
+    task.wait(Settings.Delays.WhileError)
     jumpToServer()
 end)
-task.wait(config.delays.beforeTp)
-a.Alert.Message("Tp to another server...")
+
+task.wait(Settings.Delays.BeforeTp)
+sendEventUpdate("Teleporting", "Teleporting to another server...", Settings.Webhook.Colors.Success)
 jumpToServer()
